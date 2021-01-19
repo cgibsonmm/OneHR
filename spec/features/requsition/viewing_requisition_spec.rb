cf1ddef154d995ddc6a56fec68174da4e8f766be
corey gibson <cgibsonmm@gmail.com>
2021-01-18 19:54:32 -0500

    started work on view

--- spec/features/requsition/viewing_requisition_spec.rb
+++ spec/features/requsition/viewing_requisition_spec.rb
@@ -1,1 +1,16 @@
 require 'rails_helper'
+
+RSpec.describe 'Viewing a Requisition' do
+  context 'not logged in' do
+    before do
+      @req = create(:requisition)
+    end
+
+    it 'should show the view of the requisition' do
+      visit "/requisitions/#{@req.id}"
+      expect(page).to have_content(@req.title)
+      expect(page).to have_content(@req.department)
+      expect(page).to have_content(@req.description)
+    end
+  end
+end
