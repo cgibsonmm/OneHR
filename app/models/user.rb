deb6734dbdb4d04f0e78f4f8acb5ac4268e43757
corey gibson <cgibsonmm@gmail.com>
2021-01-19 11:40:45 -0500

    added seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :requisitions
+  has_many :requisitions, dependent: :destroy
 
   def user_name
     email.split('@')[0]
