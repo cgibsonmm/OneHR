31cad1da19dc5715933d1a93fa87d2cf7f5f3a4f
corey gibson <cgibsonmm@gmail.com>
2021-02-12 13:16:21 -0500

    added mulit views

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   get 'calendar/month'
   get 'calendar/week'
-  devise_for :users
+  get 'dashboard/index'
+
   resources :requisitions
   namespace :admin do
     resources :clients
     resources :users
     resources :roles
-
     root to: 'clients#index'
   end
-  get 'dashboard/index'
+
+  devise_for :users
   devise_for :clients, path: '',
               path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'sign-up' }
-
-  root 'welcome#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
